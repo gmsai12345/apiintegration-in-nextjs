8f2a9074d4bdfdbae8a784ba39f2f8d2352230a1
gmsai12345 <saidhakshan4@gmail.com>
2023-02-12 18:44:16 +0530

    prod details page,prod page

--- pages/api/comments/[commentId].js
+++ pages/api/comments/[commentId].js
@@ -4,14 +4,14 @@ export default function handler(req,res){
     const {commentId} = req.query
     res.status(200).json(comments)
     // handle delete request
-    if (req.method === 'GET'){
+    if (req.method === 'GET'){ // this is for displaying that particular id when that is done 
         const comment = comments.find(comment => comment.id === parseInt(commentId))
 
     }
     else if(req.method === 'DELETE'){
-         const deletedcomment = comments.find(comment => comment.id === parseInt(commentId))
-         const index = comments.findIndex((comment) => comment.id === parseInt(commentId))
-         comments.splice(index, 1)
-         res.status(200).json(deletedcomment)
+         const deletedcomment = comments.find(comment => comment.id === parseInt(commentId)) // finds the commment to be deleted using id
+         const index = comments.findIndex((comment) => comment.id === parseInt(commentId)) // finding the index for the comment 
+         comments.splice(index, 1) // delete using splice
+         res.status(200).json(deletedcomment) 
     }
 }
--- pages/comments/index.js
+++ pages/comments/index.js
@@ -1,25 +1,25 @@
 import {useState} from "react"
 function index() {
     const [comments,setComments] = useState([]) // this use state is used to show the comments all comments
-    const [comment,setComment] = useState('') // this is to add
-    const fetchComments = async () => {
-        const response = await fetch('/api/comments')
-        const data = await response.json();
-        setComments(data)
+    const [comment,setComment] = useState('') // this is to add comment so that it can be published in that for update request
+    const fetchComments = async () => { // fetch all comments from comments
+        const response = await fetch('/api/comments') // wait to fetch all comments
+        const data = await response.json(); // response is converted to JSON
+        setComments(data) // then comments are displayed
     }
-    const submitComment = async () => {
-        const response = await fetch('/api/comments',{method:'POST',
-        body: JSON.stringify({comment}),
-        headers: {'Content-Type': 'application/json'}
+    const submitComment = async () => { // this is for submit comment in the page to be displayed
+        const response = await fetch('/api/comments',{method:'POST', // post request we are making
+        body: JSON.stringify({comment}), // strings the json thing written
+        headers: {'Content-Type': 'application/json'} // mandatory for every post request
     })
-    const data = await response.json()
+    const data = await response.json() // this is converted to json
         
     }
-    const deletecomment = async commentId => {
-        const response = await fetch(`/api/comments/${commentId}`,{method:'DELETE'})
-        const data = await response.json()
+    const deletecomment = async commentId => { // for delete
+        const response = await fetch(`/api/comments/${commentId}`,{method:'DELETE'}) // delete request is made to that particular id fetched 
+        const data = await response.json() // same
         console.log(data)
-        fetchComments()
+        fetchComments() 
     }
 
   return (
